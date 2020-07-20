98a7a10eede3cab2de57bf47eb72651119c46b34
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-20 00:54:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,12 @@
 def prime?(array)
-  # Add  code here!
+  if arrary <= 1 || array == 0 || array == 1 
+    return false
+    elsif
+    (2.. array - 1).each do |i|
+      if array % i == 0 
+        return false
+        end 
+        end
+        end
+    true # Add  code here!
 end
